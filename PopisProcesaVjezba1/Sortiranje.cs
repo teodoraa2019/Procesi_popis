5c513792118376049c5331b10fe51e1ce9a76e69
teodo <teodo@DESKTOP-RCIPBT2>
2019-06-17 17:55:55 +0200

    -----

---
+++ PopisProcesaVjezba1/Sortiranje.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Procesi_popis
+{
+    class Sortiranje : IComparable
+    {
+        public System.Diagnostics.Process P { get; }
+        public Sortiranje(System.Diagnostics.Process p)
+        {
+            P = p;
+        }
+        public int CompareTo(object obj)
+        {
+            Sortiranje dps = (Sortiranje) obj;
+            System.Diagnostics.Process drugiProces = dps.P;
+            if (P.WorkingSet64 > drugiProces.WorkingSet64)
+            {
+                return -1;
+            } else if (P.WorkingSet64 < drugiProces.WorkingSet64)
+            {
+                return 1;
+            } else
+            {
+                return 0;
+            }
+        }
+    }
+}
